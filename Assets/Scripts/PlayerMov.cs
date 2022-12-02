0e5b6005bc981914ed1ed3ceda50dfee9206c4fc
Matias Szamrynskyj <54959508+MatiReviews@users.noreply.github.com>
2022-12-01 21:05:35 -0300

    prefab added

---
+++ Assets/Scripts/PlayerMov.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMov : MonoBehaviour
+{
+
+    private Rigidbody2D myBody;
+
+    [SerializeField]
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start(){
+        myBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.D)){
+            transform.position += transform.right * (Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A)){
+            transform.position -= transform.right * (Time.deltaTime * speed);
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] fruits;
+    public GameObject bomb;
+
+    public float xBounds, yBounds;
+
+    // Start is called before the first frame update
+    void Start(){
+        StartCoroutine(SpawnGO());
+    }
+
+    IEnumerator SpawnGO(){
+        yield return new WaitForSeconds(Random.Range(1, 2));
+
+        int randomFruta = Random.Range(0, fruits.Length);
+
+        if (Random.value <= 0.6f){
+            Instantiate(fruits[randomFruta], new Vector2(Random.Range(-xBounds, xBounds), yBounds), Quaternion.identity);
+        }
+        else{
+            Instantiate(bomb, new Vector2(Random.Range(-xBounds, xBounds), yBounds), Quaternion.identity);
+        }
+
+        StartCoroutine(SpawnGO());
+    }    
+}
