758a3ed5ad8df997fb48d10506936adeaf2b6c0c
Matias Szamrynskyj <54959508+MatiReviews@users.noreply.github.com>
2022-12-04 20:13:37 -0300

    added final scene

---
+++ Assets/Scripts/VideoPlayerUwU.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VideoPlayerUwU : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(timer());
+    }
+
+    IEnumerator timer()
+    {
+        yield return new WaitForSeconds(16);
+        SceneManager.LoadScene(0);
+    }
+}
